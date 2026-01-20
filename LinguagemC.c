597944a04078688624b6f98487b03a271f9c87de
Pedroh54554555 <phenriqueoliveirasilva212@gmail.com>
2026-01-20 11:20:36 +0000

    Adicionei um algoritmo que pega as notas do aluno e calcula a sua média.

--- LinguagemC.c
+++ LinguagemC.c
@@ -6,17 +6,37 @@ int main(){
     char *escola = "Giovanne Barreto";
     char turma, genero;
     int idade, serie;
-    float altura, peso, media;
+    float altura, peso, media, Np, Nm, Nh, Nc, Nef, Na;
 
-    turma = 'B';
+    turma = 'C';
     genero = 'M';
-    idade = 12;
-    serie = 7;
-    altura = 1.52;
-    peso = 38.6;
-    media = 6.75;
+    idade = 10;
+    serie = 5;
+    altura = 1.42;
+    peso = 31.6;
 
-    printf("O estudante %s do genero %c, estuda na escola %s na turma %i°%c.\n", nome, genero, escola, serie, turma);
+    printf("Informe as notas do aluno %s neste bimestre:\n", nome);
+    printf("Nota de português:\n");
+    scanf("%f", &Np);
+
+    printf("Nota de matemática:\n");
+    scanf("%f", &Nm);
+
+    printf("Nota e história:\n");
+    scanf("%f", &Nh);
+
+    printf("Nota de ciências:\n");
+    scanf("%f", &Nc);
+
+    printf("Nota de educação física:\n");
+    scanf("%f", &Nef);
+
+    printf("Nota de artes\n");
+    scanf("%f", &Na);
+
+    media =(Np+Nm+Nh+Nc+Nef+Na)/6;
+
+    printf("O estudante %s do genero %c, estuda na escola %s na turma %i°%c e sua média deste bimestre foi %.2f.\n", nome, genero, escola, serie, turma, media);
     printf("Ele tem %i anos, mede %.2f metros e pesa %.1f kg.\n", idade, altura, peso);
 
 
