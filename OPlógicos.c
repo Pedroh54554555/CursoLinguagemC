0be796c6de8f452814b7ccc44b4b1b4402abaefc
Pedroh54554555 <phenriqueoliveirasilva212@gmail.com>
2026-03-09 17:04:00 +0000

    Utilização dos operadores &&(e) e ||(ou) em expressões lógicas.

---
+++ OPlógicos.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(){
+    /*
+    int a = 2, b = -1;
+
+    if(a>0 && b>0){
+        printf("Ambos são positivos!\n");
+    } else{
+        if(a>0 || b>0){
+        printf("Algum deles é negativo!\n");
+        } else{
+            printf("Os dois são negativos!\n");
+        }
+    }
+    */
+     int a = 5;
+    int b = 0;
+     int c = 0;
+
+     if (a > 0 && b < 0 || c == 0) {
+        printf("A condição é verdadeira\n");
+     } else {
+       printf("A condição é falsa\n");
+     }
+
+     return 0;
+    
+
+}
