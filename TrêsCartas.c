4458a4c7043a95d8c67910534c5a77dcf41d278b
Pedroh54554555 <phenriqueoliveirasilva212@gmail.com>
2026-04-13 17:33:04 +0000

    Algoritmo que cria três cartas com valores aleatórios.

--- TrêsCartas.c
+++ TrêsCartas.c
@@ -6,30 +6,30 @@ int main(){
     int cartaA, cartaB, cartaC, numero;
     
     srand(time(0));
-    cartaA = rand();
-    cartaB = rand();
-    cartaC = rand();
+    cartaA = 1 + rand()% 1000;
+    cartaB = 1 + rand()% 1000;
+    cartaC = 1 + rand()% 1000;
 
     printf("A seguir você tem 3 opções de cartas com valores aleatórios.\nEscolha a maior para vencer!\nEscolha entre:\nCarta A, digite 1;\nCarta B, digite 2;\nCarta C digite 3.\n");
     scanf("%i", &numero);
 
     switch (numero){
         case 1:
-            if(cartaA>cartaB && cartaA>cartaC){
+            if(cartaA>=cartaB && cartaA>=cartaC){
                 printf("Parabéns você acertou!\n");
             }else{
                 printf("Você errou, mais sorte da próxima vez!\n");
             }
             break;
         case 2:
-            if(cartaB>cartaA && cartaB>cartaC){
+            if(cartaB>=cartaA && cartaB>=cartaC){
                 printf("Parabéns você acertou!\n");
             }else{
                 printf("Você errou, mais sorte da próxima vez!\n");
             }
             break;
         case 3:
-            if(cartaC>cartaA && cartaC>cartaB){
+            if(cartaC>=cartaA && cartaC>=cartaB){
                 printf("Parabéns você acertou!\n");
             }else{
                 printf("Você errou, mais sorte da próxima vez!\n");
